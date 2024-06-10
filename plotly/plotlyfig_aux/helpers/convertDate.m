0f4191fb29a15485e6111e5af41668ded7e1e0b9
Roberto Moura <r.moura@mwam.com>
2024-06-10 16:07:20 +0100

    Improve m2json; Support strings, datetime and convert -Inf to 'null', not '-null'

--- plotly/plotlyfig_aux/helpers/convertDate.m
+++ plotly/plotlyfig_aux/helpers/convertDate.m
@@ -1,4 +1,9 @@
 function convertedDate = convertDate(date)
- date.Format = 'yyyy-MM-dd HH:mm:ss';
- convertedDate = char(date);
-end 
+format = ifel(isDate(date),'yyyy-mm-dd','yyyy-mm-dd HH:MM:ss');
+convertedDate = datestr(date,format); % works with datetime and datenum
+end
+
+function tf = isDate(x)
+    if isdatetime(x) || ischar(x), x = datenum(x); end
+    tf = all(isnan(x) | x==floor(x)); % Dates must be in whole days, without a time portion; datetime(Inf,0,0) is OK
+end
